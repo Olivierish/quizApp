5e86c727b009d784f39c9f62cdb65fbefbea1e5d
Olivier Ishimwe <olivierish@gmail.com>
2021-10-26 12:21:21 +0200

    added the results and help zone

--- app.js
+++ app.js
@@ -17,51 +17,56 @@ quizDataPromise.then(function(quizData){
     const numberOfQuestions = quizData.length;
 
     for(let i = 0; i < numberOfQuestions; i++){  
-    const cardElts = document.createElement("div");
-    cardElts.className = "card";
-    
-    //Add the audio source
-    const sourceElt = document.createElement("source");
-    sourceElt.src = "audios/" + quizData[i].audio;
-    sourceElt.type="audio/mp3";
-    const formatNotSupportedElt = document.createTextNode("Your browser does not support the audio element.");
-    const audioElt = document.createElement("audio");
-    audioElt.controls = "controls";
-    audioElt.appendChild(sourceElt);
-    audioElt.appendChild(formatNotSupportedElt);
+        const cardElts = document.createElement("div");
+        cardElts.className = "card";
+        
+        //Add the audio source
+        const sourceElt = document.createElement("source");
+        sourceElt.src = "audios/" + quizData[i].audio;
+        sourceElt.type="audio/mp3";
+        const formatNotSupportedElt = document.createTextNode("Your browser does not support the audio element.");
+        const audioElt = document.createElement("audio");
+        audioElt.controls = "controls";
+        audioElt.appendChild(sourceElt);
+        audioElt.appendChild(formatNotSupportedElt);
 
-    //Add the options section
-    const optionsElt = document.createElement("div");
-    optionsElt.className = "options";
-    
-    for(let j = 0; j < quizData[i].options.length; j++){  
-    //Add countries
-    const optionsSingleElt = document.createElement("div");
-    optionsSingleElt.className = "options-element";
+        //Add the options section
+        const optionsElt = document.createElement("div");
+        optionsElt.className = "options";
+        
+        for(let j = 0; j < quizData[i].options.length; j++){  
+            //Add countries
+            const optionsSingleElt = document.createElement("div");
+            optionsSingleElt.className = "options-element";
 
-    let country = quizData[i].options[j].country;
-    const inputElt = document.createElement("input");
-    inputElt.type = "radio";
-    inputElt.name = "q"+i;
-    inputElt.id = country;
-    inputElt.value = quizData[i].options[j].code;
-    
-    const labelElt = document.createElement("label");
-    labelElt.htmlFor = country;
+            let country = quizData[i].options[j].country;
+            const inputElt = document.createElement("input");
+            inputElt.type = "radio";
+            inputElt.name = "q"+i;
+            inputElt.id = country;
+            inputElt.value = quizData[i].options[j].code;
+            
+            const labelElt = document.createElement("label");
+            labelElt.htmlFor = country;
 
-    const imgElt = document.createElement("img");
-    imgElt.src =  "images/" + country + ".svg";
-    imgElt.alt = country+"'s national flag";
-    labelElt.appendChild(imgElt);
-    optionsSingleElt.appendChild(inputElt);
-    optionsSingleElt.appendChild(labelElt);
+            const imgElt = document.createElement("img");
+            imgElt.src =  "images/" + country + ".svg";
+            imgElt.alt = country+"'s national flag";
+            labelElt.appendChild(imgElt);
+            optionsSingleElt.appendChild(inputElt);
+            optionsSingleElt.appendChild(labelElt);
 
-    optionsElt.appendChild(optionsSingleElt);
+            optionsElt.appendChild(optionsSingleElt);
+        }
+        
+        cardElts.appendChild(audioElt);
+        cardElts.appendChild(optionsElt);
+
+        formElt.appendChild(cardElts);
+
+        let userResponses = [];
+        formElt.addEventListener('submit',e =>{
+            
+        });
     }
-    
-    cardElts.appendChild(audioElt);
-    cardElts.appendChild(optionsElt);
-
-    formElt.appendChild(cardElts);
-}
 });
